d476d02e1580a518c937b53d2de7a062787134fc
aihtnyc-h <feiver@naver.com>
2023-03-28 03:22:57 +0900

    feat: Add init 초기설정

--- src/main/java/com/ddalggak/finalproject/domain/comment/entity/Comment.java
+++ src/main/java/com/ddalggak/finalproject/domain/comment/entity/Comment.java
@@ -11,7 +11,6 @@ import javax.persistence.ManyToOne;
 
 import com.ddalggak.finalproject.domain.comment.dto.CommentRequestDto;
 import com.ddalggak.finalproject.domain.ticket.entity.Ticket;
-import com.ddalggak.finalproject.domain.user.entity.User;
 import com.ddalggak.finalproject.global.entity.BaseEntity;
 
 import lombok.Getter;
--- src/main/java/com/ddalggak/finalproject/domain/comment/service/CommentService.java
+++ src/main/java/com/ddalggak/finalproject/domain/comment/service/CommentService.java
@@ -10,7 +10,6 @@ import com.ddalggak.finalproject.domain.ticket.entity.Ticket;
 
 import com.ddalggak.finalproject.domain.comment.repository.CommentRepository;
 import com.ddalggak.finalproject.domain.ticket.repository.TicketRepository;
-import com.ddalggak.finalproject.domain.user.entity.User;
 import com.ddalggak.finalproject.global.dto.SuccessCode;
 import com.ddalggak.finalproject.global.dto.SuccessResponseDto;
 import com.ddalggak.finalproject.global.error.CustomException;
