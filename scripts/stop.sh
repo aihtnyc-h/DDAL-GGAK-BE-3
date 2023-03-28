f32025bfbcb43600ee3119db5f2422ebbdbbce80
aihtnyc <111184537+aihtnyc-h@users.noreply.github.com>
2023-03-28 18:43:54 +0900

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -3,7 +3,7 @@
 PROJECT_ROOT="/home/ubuntu/app"
 JAR_FILE="$PROJECT_ROOT/DDAL-GGAK-BE-0.0.1-SNAPSHOT.jar"
 
-DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
+# DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
 TIME_NOW=$(date +%c)
 
