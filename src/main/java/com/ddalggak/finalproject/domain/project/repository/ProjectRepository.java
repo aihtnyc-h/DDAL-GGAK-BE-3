c77829fcc9389a790a49fdff9312d5bb4ee8ce50
Cho Youngjun <108874833+choincnp@users.noreply.github.com>
2023-04-02 14:37:55 +0900

    Update ProjectRepository.java

--- src/main/java/com/ddalggak/finalproject/domain/project/repository/ProjectRepository.java
+++ src/main/java/com/ddalggak/finalproject/domain/project/repository/ProjectRepository.java
@@ -12,12 +12,4 @@ import com.ddalggak.finalproject.domain.project.entity.Project;
 
 public interface ProjectRepository extends JpaRepository<Project, Long>, ProjectRepositoryCustom {
 
-	//나중에 projectUser에 기능 더 붙으면, 아니면 user의 정보조회까지 필요하면 fetch join이 쿼리 절약해줄 수 있음
-	@Query("select new com.ddalggak.finalproject.domain.project.dto.ProjectBriefResponseDto(p.projectId, p.projectTitle, p.thumbnail) from Project p join p.projectUserList pu where pu.user.userId = :userId")
-	List<ProjectBriefResponseDto> findAllinDtoByUserId(Long userId);
-
-	@Modifying
-	@Query("update Project p set p.projectTitle = :projectTitle, p.thumbnail = :thumbnail where p.projectId = :projectId")
-	void update(@Param("projectTitle") String projectTitle, @Param("thumbnail") String thumbnail,
-		Long projectId);
 }
