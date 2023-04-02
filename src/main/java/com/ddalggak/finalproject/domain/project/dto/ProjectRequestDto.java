434a38aa87994e162777f7b209b9f393079ac2d6
Cho Youngjun <108874833+choincnp@users.noreply.github.com>
2023-04-02 14:45:55 +0900

    Update ProjectRequestDto.java

--- src/main/java/com/ddalggak/finalproject/domain/project/dto/ProjectRequestDto.java
+++ src/main/java/com/ddalggak/finalproject/domain/project/dto/ProjectRequestDto.java
@@ -16,7 +16,8 @@ public class ProjectRequestDto {
 	@NotBlank(message = "프로젝트 이름은 필수 입력값입니다.")
 	public String projectTitle;
 
-	@Schema(name = "프로젝트 썸네일", example = "http://ddalggak.ap-northeast-1.amazonaws.com/thumbnail/projects/~")
+	@Schema(name = "프로젝트 썸네일")
+	@Setter
 	public String thumbnail;
 	@Schema(name = "프로젝트 썸네일 이미지", example = "project.jpg")
 	private String imageUrl;
