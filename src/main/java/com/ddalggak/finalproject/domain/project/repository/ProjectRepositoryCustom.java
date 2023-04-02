f47664404139df390b3a61ebec1adbea722e71c5
Cho Youngjun <108874833+choincnp@users.noreply.github.com>
2023-04-02 14:38:14 +0900

    Update ProjectRepositoryCustom.java

--- src/main/java/com/ddalggak/finalproject/domain/project/repository/ProjectRepositoryCustom.java
+++ src/main/java/com/ddalggak/finalproject/domain/project/repository/ProjectRepositoryCustom.java
@@ -7,4 +7,8 @@ import com.ddalggak.finalproject.domain.project.dto.ProjectBriefResponseDto;
 public interface ProjectRepositoryCustom {
 
 	List<ProjectBriefResponseDto> findProjectAllByUserId(Long userId);
+
+	ProjectResponseDto findDtoByProjectId(Long projectId);
+
+	void update(Long projectId, ProjectRequestDto projectRequestDto);
 }
