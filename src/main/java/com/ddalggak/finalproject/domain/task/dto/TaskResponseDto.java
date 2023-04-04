2dc63baa819594f180d941776a80d653343af315
Cho Youngjun <whdudwns7777@gmail.com>
2023-04-04 10:59:11 +0900

    feat: Add jsonView and logics

--- src/main/java/com/ddalggak/finalproject/domain/task/dto/TaskResponseDto.java
+++ src/main/java/com/ddalggak/finalproject/domain/task/dto/TaskResponseDto.java
@@ -2,6 +2,7 @@ package com.ddalggak.finalproject.domain.task.dto;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,9 @@ import com.ddalggak.finalproject.domain.label.dto.LabelResponseDto;
 import com.ddalggak.finalproject.domain.task.entity.Task;
 import com.ddalggak.finalproject.domain.ticket.dto.TicketResponseDto;
 import com.ddalggak.finalproject.domain.ticket.entity.Ticket;
+import com.ddalggak.finalproject.domain.ticket.entity.TicketStatus;
+import com.ddalggak.finalproject.global.view.Views;
+import com.fasterxml.jackson.annotation.JsonView;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
@@ -17,28 +21,36 @@ import lombok.Builder;
 public class TaskResponseDto {
 
 	@Schema(name = "task id", example = "1")
+	@JsonView(Views.Task.class)
 	public Long id;
 
 	@Schema(name = "task title", example = "task title")
+	@JsonView(Views.Task.class)
 	public String taskTitle;
 
 	@Schema(name = "task leader", example = "task leader")
+	@JsonView(Views.Task.class)
 	public String taskLeader;
 
 	@Schema(name = "when does this project expired at", example = "2023-03-22")
+	@JsonView(Views.Task.class)
 	public LocalDate expiredAt;
 
 	@Schema(name = "total difficulty", example = "10", defaultValue = "0")
+	@JsonView(Views.Task.class)
 	public int totalDifficulty;
 
 	@Schema(name = "total priority", example = "10", defaultValue = "0")
+	@JsonView(Views.Task.class)
 	public int totalPriority;
 
 	@Schema(name = "labels", example = "front-end", defaultValue = "null")
+	@JsonView(Views.Task.class)
 	public List<LabelResponseDto> labels;
 
 	@Schema(name = "total tickets")
-	public List<TicketResponseDto> tickets;
+	@JsonView(Views.Task.class)
+	public Map<TicketStatus, List<TicketResponseDto>> tickets;
 
 	@Builder
 	public TaskResponseDto(Task task) {
@@ -49,7 +61,10 @@ public class TaskResponseDto {
 		totalDifficulty = task.getTicketList().stream().mapToInt(Ticket::getDifficulty).sum();
 		totalPriority = task.getTicketList().stream().mapToInt(Ticket::getPriority).sum();
 		labels = task.getLabelList().stream().map(LabelResponseDto::of).collect(Collectors.toList());
-		tickets = task.getTicketList().stream().map(TicketResponseDto::of).collect(Collectors.toList());
+		tickets = task.getTicketList()
+			.stream()
+			.map(TicketResponseDto::new)
+			.collect(Collectors.groupingBy(TicketResponseDto::getStatus));
 	}
 
 	// todo global response Dto 만들어서 전부 data에 넣기
