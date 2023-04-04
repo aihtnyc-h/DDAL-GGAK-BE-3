bb2f6cce8cc203971b1a17fb8ed0985d429ac2b8
Cho Youngjun <whdudwns7777@gmail.com>
2023-04-04 10:58:41 +0900

    feat: Add annotations

--- src/main/java/com/ddalggak/finalproject/domain/task/dto/TaskRequestDto.java
+++ src/main/java/com/ddalggak/finalproject/domain/task/dto/TaskRequestDto.java
@@ -5,9 +5,13 @@ import java.time.LocalDate;
 import javax.validation.constraints.NotNull;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
+@AllArgsConstructor
 public class TaskRequestDto {
 
 	@Schema(name = "project Id", example = "project Id")
