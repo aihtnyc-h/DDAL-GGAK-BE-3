7f8444fc290d904f2358d5302183383756baf206
aihtnyc <111184537+aihtnyc-h@users.noreply.github.com>
2023-04-05 19:21:20 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -22,17 +22,17 @@ TIME_NOW=$(date +%c)
 # echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
 
 
-# 현재 구동중인 어플리케이션 PID 확인
-CURRENT_PID=$(pgrep -f $JAR_FILE)
-echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
+# # 현재 구동중인 어플리케이션 PID 확인
+# CURRENT_PID=$(pgrep -f $JAR_FILE)
+# echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
 
-if [ -z $CURRENT_PID ]; then
-  echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다" >> $DEPLOY_LOG
-else
-  echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션 종료 " >> $DEPLOY_LOG
-  kill -15 $CURRENT_PID
-  sleep 5
-fi
+# if [ -z $CURRENT_PID ]; then
+#   echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다" >> $DEPLOY_LOG
+# else
+#   echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션 종료 " >> $DEPLOY_LOG
+#   kill -15 $CURRENT_PID
+#   sleep 5
+# fi
 
 # build 파일 복사
 echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
