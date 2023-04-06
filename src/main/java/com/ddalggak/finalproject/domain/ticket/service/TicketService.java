17fd1e0bb8e697e3b8c4af5771bffc9a83d3dccf
Cho Youngjun <whdudwns7777@gmail.com>
2023-04-06 16:05:17 +0900

    fix: response data 작업 완료

--- src/main/java/com/ddalggak/finalproject/domain/ticket/service/TicketService.java
+++ src/main/java/com/ddalggak/finalproject/domain/ticket/service/TicketService.java
@@ -46,7 +46,8 @@ public class TicketService {
 		//4. 티켓 저장
 		ticketRepository.save(ticket);
 		//5. return
-		return SuccessResponseDto.toResponseEntity(SuccessCode.CREATED_SUCCESSFULLY);
+		return ResponseEntity.status(201)
+			.body(ticketRepository.findWithTaskId(ticketRequestDto.getTaskId()));
 	}
 
 	// 티켓 상세조회
