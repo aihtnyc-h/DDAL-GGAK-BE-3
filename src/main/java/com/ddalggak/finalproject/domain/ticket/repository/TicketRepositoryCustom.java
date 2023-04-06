10b84f04fe2081873260145d6cd972df21c62dcb
Cho Youngjun <whdudwns7777@gmail.com>
2023-04-06 16:05:32 +0900

    fix: response data 작업 완료

--- src/main/java/com/ddalggak/finalproject/domain/ticket/repository/TicketRepositoryCustom.java
+++ src/main/java/com/ddalggak/finalproject/domain/ticket/repository/TicketRepositoryCustom.java
@@ -1,9 +1,12 @@
 package com.ddalggak.finalproject.domain.ticket.repository;
 
+import java.util.List;
+
 import com.ddalggak.finalproject.domain.ticket.dto.TicketResponseDto;
 
 public interface TicketRepositoryCustom {
 	TicketResponseDto findWithOrderedComments(Long ticketId);
 
+	List<TicketResponseDto> findWithTaskId(Long taskId);
 	// void update(Long ticketId, TicketRequestDto ticketRequestDto);
 }
