e91bb16de89ae8345a97607505d54347dd0917d6
Cho Youngjun <whdudwns7777@gmail.com>
2023-04-07 21:38:03 +0900

    feat: 라벨별 티켓조회 로직 생성

--- src/main/java/com/ddalggak/finalproject/domain/ticket/repository/TicketRepositoryCustom.java
+++ src/main/java/com/ddalggak/finalproject/domain/ticket/repository/TicketRepositoryCustom.java
@@ -1,12 +1,17 @@
 package com.ddalggak.finalproject.domain.ticket.repository;
 
 import java.util.List;
+import java.util.Map;
 
 import com.ddalggak.finalproject.domain.ticket.dto.TicketResponseDto;
+import com.ddalggak.finalproject.domain.ticket.entity.TicketStatus;
 
 public interface TicketRepositoryCustom {
 	TicketResponseDto findWithOrderedComments(Long ticketId);
 
 	List<TicketResponseDto> findWithTaskId(Long taskId);
 	// void update(Long ticketId, TicketRequestDto ticketRequestDto);
+
+	Map<TicketStatus, List<TicketResponseDto>> findWithLabelId(Long labelId);
+
 }
