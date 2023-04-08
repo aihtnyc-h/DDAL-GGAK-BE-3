3aeb9e0bfdb07126190a4842ab966e32974f1a9a
Cho Youngjun <whdudwns7777@gmail.com>
2023-04-09 03:15:14 +0900

    feat: Dto관련로직 Mapper로 이동

--- src/main/java/com/ddalggak/finalproject/domain/comment/dto/CommentResponseDto.java
+++ src/main/java/com/ddalggak/finalproject/domain/comment/dto/CommentResponseDto.java
@@ -1,10 +1,9 @@
 package com.ddalggak.finalproject.domain.comment.dto;
 
-import com.ddalggak.finalproject.domain.comment.entity.Comment;
 import com.ddalggak.finalproject.global.view.Views;
 import com.fasterxml.jackson.annotation.JsonView;
-import com.querydsl.core.annotations.QueryProjection;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +11,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @Builder
+@AllArgsConstructor
 public class CommentResponseDto {
 
 	@JsonView(Views.Ticket.class)
@@ -20,17 +20,4 @@ public class CommentResponseDto {
 	private String comment;
 	@JsonView(Views.Ticket.class)
 	private String email;
-
-	@QueryProjection
-	public CommentResponseDto(Long commentId, String comment, String email) {
-		this.commentId = commentId;
-		this.comment = comment;
-		this.email = email;
-	}
-
-	public CommentResponseDto(Comment c) {
-		this.commentId = c.getCommentId();
-		this.comment = c.getComment();
-		this.email = c.getUser().getEmail();
-	}
 }
