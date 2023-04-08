687280dfeaa9379aaa8491762847aeaea6c29b20
Cho Youngjun <whdudwns7777@gmail.com>
2023-04-09 05:53:39 +0900

    feat: Repository 새로운 메서드 생성

--- src/main/java/com/ddalggak/finalproject/domain/user/repository/UserRepositoryCustom.java
+++ src/main/java/com/ddalggak/finalproject/domain/user/repository/UserRepositoryCustom.java
@@ -3,6 +3,7 @@ package com.ddalggak.finalproject.domain.user.repository;
 import java.util.List;
 
 import com.ddalggak.finalproject.domain.label.entity.LabelUser;
+import com.ddalggak.finalproject.domain.project.entity.ProjectUser;
 import com.ddalggak.finalproject.domain.task.entity.TaskUser;
 import com.ddalggak.finalproject.domain.ticket.dto.DateTicket;
 import com.ddalggak.finalproject.domain.ticket.dto.TicketSearchCondition;
@@ -20,5 +21,9 @@ public interface UserRepositoryCustom {
 	List<TaskUser> getTaskUserFromTaskId(Long taskId);
 
 	List<User> getUserFromTaskId(Long taskId);
+
+	List<User> getUserFromProjectId(Long projectId);
+
+	List<ProjectUser> getProjectUserFromProjectId(Long projectId);
 
 }
