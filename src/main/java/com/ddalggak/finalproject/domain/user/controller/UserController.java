77967d2417714dd445592d53018aae308f3cca1c
SeongMinnnn <122071252+SeongMinnnn@users.noreply.github.com>
2023-04-11 10:10:44 +0900

    fix: 코드정리

--- src/main/java/com/ddalggak/finalproject/domain/user/controller/UserController.java
+++ src/main/java/com/ddalggak/finalproject/domain/user/controller/UserController.java
@@ -3,8 +3,6 @@ package com.ddalggak.finalproject.domain.user.controller;
 import java.io.IOException;
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
@@ -27,25 +25,21 @@ import com.ddalggak.finalproject.domain.user.dto.UserPageDto;
 import com.ddalggak.finalproject.domain.user.exception.UserException;
 import com.ddalggak.finalproject.domain.user.repository.UserRepository;
 import com.ddalggak.finalproject.domain.user.service.UserService;
-import com.ddalggak.finalproject.global.dto.SuccessCode;
-import com.ddalggak.finalproject.global.dto.SuccessResponseDto;
 import com.ddalggak.finalproject.global.error.ErrorCode;
-import com.ddalggak.finalproject.global.error.ErrorResponse;
 import com.ddalggak.finalproject.global.jwt.JwtUtil;
 import com.ddalggak.finalproject.global.security.UserDetailsImpl;
 
-import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/user")
 @RequiredArgsConstructor
 public class UserController {
 	private final UserService userService;
 	private final JwtUtil jwtUtil;
 	private final UserRepository userRepository;
 
-	@PutMapping("/user/nickname")
+	@PutMapping("/nickname")
 	public NicknameDto updateNickname(@Valid @RequestBody NicknameDto nicknameDto,
 		@AuthenticationPrincipal UserDetailsImpl userDetails, BindingResult bindingResult) {
 		if (bindingResult.hasErrors()) {
@@ -58,37 +52,20 @@ public class UserController {
 		return userService.updateNickname(nicknameDto.getNickname(), userDetails.getEmail());
 	}
 
-	@PutMapping("/user/profile")
+	@PutMapping("/profile")
 	public ProfileDto updateProfile(@RequestPart(value = "image") MultipartFile image,
 		@AuthenticationPrincipal UserDetailsImpl userDetails) throws IOException {
 		return userService.updateProfile(image, userDetails.getEmail());
 	}
 
-	@GetMapping("/user")
+	@GetMapping
 	public ResponseEntity<UserPageDto> getMyPage(@AuthenticationPrincipal UserDetailsImpl userDetails) {
 		return userService.getMyPage(userDetails.getEmail());
 	}
 
-	@GetMapping("/user/{userId}/Tickets")
+	@GetMapping("/{userId}/Tickets")
 	public ResponseEntity<?> getMyTickets(@PathVariable Long userId,
 		TicketSearchCondition condition) {
 		return userService.getMyTickets(userId, condition);
-	}
-
-	@GetMapping("/auth/validToken")
-	public ResponseEntity<?> validateToken(HttpServletRequest request, HttpServletResponse response) {
-		String token = jwtUtil.resolveToken(request);
-		Claims claims;
-		if (token != null) {
-			if (jwtUtil.validateToken(token)) {
-				claims = jwtUtil.getUserInfo(token);
-			} else {
-				return ErrorResponse.from(ErrorCode.INVALID_REQUEST);
-			}
-			userRepository.findByEmail(claims.getSubject())
-				.orElseThrow(() -> new IllegalArgumentException("사용자가 존재하지 않습니다."));
-		} else
-			throw new UserException(ErrorCode.INVALID_REQUEST);
-		return SuccessResponseDto.of(SuccessCode.SUCCESS_AUTH);
 	}
 }
