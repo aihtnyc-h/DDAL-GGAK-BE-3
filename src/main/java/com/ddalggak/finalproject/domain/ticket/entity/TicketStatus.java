2d1bf24d935949b3d68228de60a1239426bc3926
aihtnyc-h <feiver@naver.com>
2023-04-12 20:46:11 +0900

    feat: Fix review

--- src/main/java/com/ddalggak/finalproject/domain/ticket/entity/TicketStatus.java
+++ src/main/java/com/ddalggak/finalproject/domain/ticket/entity/TicketStatus.java
@@ -3,5 +3,8 @@ package com.ddalggak.finalproject.domain.ticket.entity;
 public enum TicketStatus {
 	TODO,
 	IN_PROGRESS,
-	DONE;
+	DONE,
+	REVIEW_BEFORE_PROGRESS,
+	REVIEW_IN_PROGRESS,
+	REVIEW_DONE;
 }
