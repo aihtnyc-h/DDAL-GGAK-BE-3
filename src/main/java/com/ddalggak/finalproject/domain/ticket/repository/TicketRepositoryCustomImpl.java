8bbe48265c4fd7433cb69626120bbf6aa0b2950c
Cho Youngjun <whdudwns7777@gmail.com>
2023-04-12 15:58:15 +0900

    fix: 로직 수행 위치 수정

--- src/main/java/com/ddalggak/finalproject/domain/ticket/repository/TicketRepositoryCustomImpl.java
+++ src/main/java/com/ddalggak/finalproject/domain/ticket/repository/TicketRepositoryCustomImpl.java
@@ -5,10 +5,21 @@ import static com.ddalggak.finalproject.domain.label.entity.QLabel.*;
 import static com.ddalggak.finalproject.domain.task.entity.QTask.*;
 import static com.ddalggak.finalproject.domain.ticket.entity.QTicket.*;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
+
+import com.ddalggak.finalproject.domain.ticket.dto.DateTicket;
+import com.ddalggak.finalproject.domain.ticket.dto.QDateTicket;
+import com.ddalggak.finalproject.domain.ticket.dto.TicketSearchCondition;
 import com.ddalggak.finalproject.domain.ticket.entity.Ticket;
+import com.ddalggak.finalproject.domain.ticket.entity.TicketStatus;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import lombok.RequiredArgsConstructor;
@@ -47,5 +58,72 @@ public class TicketRepositoryCustomImpl implements TicketRepositoryCustom {
 			.where(ticket.label.labelId.eq(labelId))
 			.orderBy(ticket.createdAt.desc())
 			.fetch();
+	}
+
+	@Override // todo 365개 뿌리기
+	public List<DateTicket> getCompletedTicketCountByDate(TicketSearchCondition condition, Long userId) {
+		return queryFactory
+			.select(new QDateTicket(
+				ticket.completedAt.as("date"),
+				ticket.count().as("completedTicket")
+			))
+			.from(ticket)
+			.where(ticket.user.userId.eq(userId),
+				getWithOneYear(condition.getDate())
+			)
+			.groupBy(ticket.completedAt)
+			.orderBy(ticket.completedAt.asc())
+			.limit(365)
+			.fetch();
+	}
+
+	@Override // todo 무한스크롤 이용
+	public Slice<DateTicket> getSlicedCompletedTicketCountByDate(TicketSearchCondition condition, Pageable pageable,
+		Long userId) {
+		List<DateTicket> content = new ArrayList<>();
+		queryFactory
+			.select(new QDateTicket(
+				ticket.completedAt.as("date"),
+				ticket.count().as("completedTicket")
+			))
+			.from(ticket)
+			.where(ticket.user.userId.eq(userId),
+				getWithOneYear(condition.getDate())
+			)
+			.groupBy(ticket.completedAt)
+			.orderBy(ticket.completedAt.asc())
+			.offset(pageable.getOffset())
+			.limit(pageable.getPageSize() + 1)
+			.fetch()
+			.forEach(
+				dateTicket -> content.add(new DateTicket(dateTicket.getDate(), dateTicket.getCompletedTicket()))
+			);
+		boolean hasNext = false;
+		if (content.size() > pageable.getPageSize()) {
+			content.remove(pageable.getPageSize());
+			hasNext = true;
+		}
+		return new SliceImpl<>(content, pageable, hasNext);
+
+	}
+
+	@Override
+	public List<Ticket> getTicketByUserId(TicketSearchCondition condition, Long userId) {
+		return queryFactory
+			.selectFrom(ticket)
+			.where(ticket.user.userId.eq(userId),
+				statusEq(condition.getStatus())
+			)
+			.orderBy(ticket.createdAt.desc())
+			.fetch();
+
+	}
+
+	private BooleanExpression getWithOneYear(LocalDate localDate) {
+		return localDate != null ? ticket.completedAt.between(localDate.minusYears(1), localDate) : null;
+	}
+
+	private BooleanExpression statusEq(TicketStatus status) {
+		return status != null ? ticket.status.eq(status) : null;
 	}
 }
