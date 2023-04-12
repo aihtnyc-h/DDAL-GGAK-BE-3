8a1e4ac47e0ecc25358820f313d6aeea9c2f702f
Cho Youngjun <whdudwns7777@gmail.com>
2023-04-13 01:05:39 +0900

    feat: JsonView 제거

--- src/main/java/com/ddalggak/finalproject/domain/label/dto/LabelResponseDto.java
+++ src/main/java/com/ddalggak/finalproject/domain/label/dto/LabelResponseDto.java
@@ -1,8 +1,5 @@
 package com.ddalggak.finalproject.domain.label.dto;
 
-import com.ddalggak.finalproject.global.view.Views;
-import com.fasterxml.jackson.annotation.JsonView;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,10 +8,6 @@ import lombok.Getter;
 @Builder
 @AllArgsConstructor
 public class LabelResponseDto {
-
-	@JsonView(Views.Task.class)
 	private Long labelId;
-
-	@JsonView(Views.Task.class)
 	private String labelTitle;
 }
