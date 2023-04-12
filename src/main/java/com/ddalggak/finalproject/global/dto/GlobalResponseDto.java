8512269a9051aed8645e267ead2f6e4745ea4bec
Cho Youngjun <whdudwns7777@gmail.com>
2023-04-12 20:35:39 +0900

    fix : return값 일관적으로 수정하게 하는 response 제작

--- src/main/java/com/ddalggak/finalproject/global/dto/GlobalResponseDto.java
+++ src/main/java/com/ddalggak/finalproject/global/dto/GlobalResponseDto.java
@@ -1,9 +1,7 @@
 package com.ddalggak.finalproject.global.dto;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
-import org.springframework.hateoas.Link;
 import org.springframework.http.ResponseEntity;
 
 import lombok.AllArgsConstructor;
@@ -18,30 +16,17 @@ import lombok.Setter;
 public class GlobalResponseDto<T> {
 
 	private final LocalDateTime timestamp = LocalDateTime.now();
-
 	private final int status;
-
 	private final String message;
-
 	private T data;
 
-	private List<Link> links;
-
-	public GlobalResponseDto(SuccessCode successCode, T data, List<Link> links) {
-		this.status = successCode.getHttpStatus().value();
-		this.message = successCode.getDetail();
-		this.data = data;
-		this.links = links;
-	}
-
-	public static <T> ResponseEntity<GlobalResponseDto<T>> of(SuccessCode successCode, T data, List<Link> links) {
+	public static <T> ResponseEntity<GlobalResponseDto<T>> of(SuccessCode successCode, T data) {
 		return ResponseEntity
 			.status(successCode.getHttpStatus())
 			.body(GlobalResponseDto.<T>builder()
 				.status(successCode.getHttpStatus().value())
 				.message(successCode.getDetail())
 				.data(data)
-				.links(links)
 				.build()
 			);
 	}
