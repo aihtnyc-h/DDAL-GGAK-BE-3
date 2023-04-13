05f9ea63c4cfa9adc057a3456d4ad1ae43a7f4ab
aihtnyc-h <feiver@naver.com>
2023-04-14 05:05:02 +0900

    feat: Add nginx test code

--- src/main/java/com/ddalggak/finalproject/global/nginx/WebRestController.java
+++ src/main/java/com/ddalggak/finalproject/global/nginx/WebRestController.java
@@ -1,32 +1,11 @@
-package com.ddalggak.finalproject.global.nginx;
-
-import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import lombok.RequiredArgsConstructor;
-
-@RestController
-@RequiredArgsConstructor
-public class WebRestController {
-
-	private final Environment env;
-
-	@GetMapping("/profile")
-	public String getProfile() {
-		String[] str = env.getActiveProfiles();
-		int idx = 0;
-		for(int i = 0; i < str.length; i++){
-			if(str[i].contains("set")){
-				idx = i;
-				break;
-			}
-		}
-
-		return str[idx];
-	}
-}
-
+// package com.ddalggak.finalproject.global.nginx;
+//
+// import org.springframework.core.env.Environment;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RestController;
+//
+// import lombok.RequiredArgsConstructor;
+//
 // @RestController
 // @RequiredArgsConstructor
 // public class WebRestController {
@@ -36,11 +15,7 @@ public class WebRestController {
 // 	@GetMapping("/profile")
 // 	public String getProfile() {
 // 		String[] str = env.getActiveProfiles();
-// 		if (str.length == 0) {
-// 			return "No active profiles found";
-// 		}
-//
-// 		int idx = -1;
+// 		int idx = 0;
 // 		for(int i = 0; i < str.length; i++){
 // 			if(str[i].contains("set")){
 // 				idx = i;
@@ -48,10 +23,35 @@ public class WebRestController {
 // 			}
 // 		}
 //
-// 		if (idx >= 0) {
-// 			return str[idx];
-// 		} else {
-// 			return "No active profile containing 'set' found";
-// 		}
+// 		return str[idx];
 // 	}
 // }
+//
+// // @RestController
+// // @RequiredArgsConstructor
+// // public class WebRestController {
+// //
+// // 	private final Environment env;
+// //
+// // 	@GetMapping("/profile")
+// // 	public String getProfile() {
+// // 		String[] str = env.getActiveProfiles();
+// // 		if (str.length == 0) {
+// // 			return "No active profiles found";
+// // 		}
+// //
+// // 		int idx = -1;
+// // 		for(int i = 0; i < str.length; i++){
+// // 			if(str[i].contains("set")){
+// // 				idx = i;
+// // 				break;
+// // 			}
+// // 		}
+// //
+// // 		if (idx >= 0) {
+// // 			return str[idx];
+// // 		} else {
+// // 			return "No active profile containing 'set' found";
+// // 		}
+// // 	}
+// // }
---
+++ src/test/java/com/ddalggak/finalproject/global/nginx/MyController.java
@@ -0,0 +1,16 @@
+package com.ddalggak.finalproject.global.nginx;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class MyController {
+
+	@GetMapping("/test")
+	public ResponseEntity<String> test() {
+		return ResponseEntity.ok("Hello, world!");
+	}
+}
