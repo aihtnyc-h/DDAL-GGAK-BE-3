56c7bbe0206045ff5f15d4d6c4be3dacf98fc7f1
aihtnyc <111184537+aihtnyc-h@users.noreply.github.com>
2023-04-13 18:12:25 +0900

    Update CommentRepository.java

--- src/main/java/com/ddalggak/finalproject/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/ddalggak/finalproject/domain/comment/repository/CommentRepository.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ddalggak.finalproject.domain.comment.entity.Comment;
-import com.ddalggak.finalproject.domain.ticket.entity.Ticket;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 	List<Comment> findAllByTicketOrderByCreatedAtDesc(Ticket ticket);
