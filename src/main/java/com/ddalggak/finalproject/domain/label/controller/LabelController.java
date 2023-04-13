f1d33ed87ca963a60718f25042b4b1cbf2933250
aihtnyc <111184537+aihtnyc-h@users.noreply.github.com>
2023-04-13 10:10:09 +0900

    Update LabelController.java

--- src/main/java/com/ddalggak/finalproject/domain/label/controller/LabelController.java
+++ src/main/java/com/ddalggak/finalproject/domain/label/controller/LabelController.java
@@ -3,6 +3,7 @@ package com.ddalggak.finalproject.domain.label.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,9 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ddalggak.finalproject.domain.label.dto.LabelRequestDto;
-import com.ddalggak.finalproject.domain.label.dto.LabelResponseDto;
 import com.ddalggak.finalproject.domain.label.service.LabelService;
-import com.ddalggak.finalproject.global.dto.SuccessResponseDto;
 import com.ddalggak.finalproject.global.security.UserDetailsImpl;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -28,7 +27,7 @@ public class LabelController {
 
 	@Operation(summary = "Label 생성", description = "api for creating label")
 	@PostMapping("/label")
-	public ResponseEntity<LabelResponseDto> createLabel(
+	public ResponseEntity<?> createLabel(
 		@AuthenticationPrincipal UserDetailsImpl userDetails,
 		@RequestBody LabelRequestDto labelRequestDto) {
 		return labelService.createLabel(userDetails.getUser(), labelRequestDto);
@@ -36,16 +35,15 @@ public class LabelController {
 
 	@Operation(summary = "Label 삭제", description = "api for delete label")
 	@DeleteMapping("/label/{labelId}")
-	public ResponseEntity<SuccessResponseDto> deleteLabel(
+	public ResponseEntity<?> deleteLabel(
 		@AuthenticationPrincipal UserDetailsImpl userDetails,
-		@RequestBody LabelRequestDto labelRequestDto,
 		@PathVariable Long labelId) {
-		return labelService.deleteLabel(userDetails.getUser(), labelRequestDto.taskId, labelId);
+		return labelService.deleteLabel(userDetails.getUser(), labelId);
 	}
 
 	@Operation(summary = "Label 강제입성", description = "api for invite label")
 	@PostMapping("/label/{labelId}/invite")
-	public ResponseEntity<SuccessResponseDto> inviteLabel(
+	public ResponseEntity<?> inviteLabel(
 		@AuthenticationPrincipal UserDetailsImpl userDetails,
 		@RequestBody LabelRequestDto labelRequestDto,
 		@PathVariable Long labelId) {
@@ -54,11 +52,19 @@ public class LabelController {
 
 	@Operation(summary = "Label 리더 부여", description = "api for assign admin to label")
 	@PostMapping("/label/{labelId}/leader")
-	public ResponseEntity<SuccessResponseDto> assignLeader(
+	public ResponseEntity<?> assignLeader(
 		@AuthenticationPrincipal UserDetailsImpl userDetails,
 		@RequestBody LabelRequestDto labelRequestDto,
 		@PathVariable Long labelId) {
 		return labelService.assignLeader(userDetails.getUser(), labelRequestDto, labelId);
+	}
+
+	@Operation(summary = "Label별 티켓 조회", description = "api for get ticketList by label")
+	@GetMapping("/label/{labelId}")
+	public ResponseEntity<?> getTicketListByLabel(
+		@AuthenticationPrincipal UserDetailsImpl userDetails,
+		@PathVariable Long labelId) {
+		return labelService.getTicketListByLabel(userDetails.getUser(), labelId);
 	}
 
 }
