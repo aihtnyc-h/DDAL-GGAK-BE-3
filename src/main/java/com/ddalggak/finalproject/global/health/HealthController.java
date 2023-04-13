f749e7f8556ea0305c94b2d250e1f21e8a71e42a
aihtnyc-h <feiver@naver.com>
2023-04-14 05:05:37 +0900

    feat: fix: health check 오타 수정

--- src/main/java/com/ddalggak/finalproject/global/health/HealthController.java
+++ src/main/java/com/ddalggak/finalproject/global/health/HealthController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class HealthController {
 	@GetMapping("/health")
-	public ResponseEntity<String> healtCheck() {
+	public ResponseEntity<String> healthCheck() {
 		return ResponseEntity.ok("OK!");
 	}
 }
