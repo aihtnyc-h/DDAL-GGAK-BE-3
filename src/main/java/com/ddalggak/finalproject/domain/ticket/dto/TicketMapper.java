260fd0834ed5d889189dddffab1950c9cc5a01ef
Cho Youngjun <whdudwns7777@gmail.com>
2023-04-15 02:37:11 +0900

    feat: 티켓 진행률 표시

--- src/main/java/com/ddalggak/finalproject/domain/ticket/dto/TicketMapper.java
+++ src/main/java/com/ddalggak/finalproject/domain/ticket/dto/TicketMapper.java
@@ -2,22 +2,22 @@ package com.ddalggak.finalproject.domain.ticket.dto;
 
 import static com.ddalggak.finalproject.domain.ticket.entity.TicketStatus.*;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
 import org.mapstruct.Mappings;
 import org.mapstruct.Named;
 
 import com.ddalggak.finalproject.domain.comment.dto.CommentMapper;
+import com.ddalggak.finalproject.domain.task.entity.Task;
 import com.ddalggak.finalproject.domain.ticket.entity.Ticket;
 import com.ddalggak.finalproject.domain.ticket.entity.TicketStatus;
-import com.ddalggak.finalproject.domain.user.entity.User;
 
 @Mapper(componentModel = "spring", uses = {CommentMapper.class})
 public interface TicketMapper {
@@ -28,11 +28,12 @@ public interface TicketMapper {
 		@Mapping(target = "description", source = "entity.ticketDescription"),
 		@Mapping(target = "priority", source = "entity.priority"),
 		@Mapping(target = "difficulty", source = "entity.difficulty"),
-		@Mapping(target = "assigned", expression = "java(checkAssigned(entity))"),
+		@Mapping(target = "assigned", source = "entity.user.email"),
 		@Mapping(target = "expiredAt", source = "entity.expiredAt"),
-		// @Mapping(target = "review_status", source = "entity.expiredAt"),
 		@Mapping(target = "label", source = "entity.label.labelTitle"),
-		@Mapping(target = "commentList", source = "entity.comment")
+		@Mapping(target = "commentList", source = "entity.comment"),
+		@Mapping(target = "progress", expression = "java(checkProgress(entity))"),
+		@Mapping(target = "score", expression = "java(calculateScore(entity))")
 	})
 	TicketResponseDto toDto(Ticket entity);
 
@@ -53,24 +54,72 @@ public interface TicketMapper {
 		return ticketList;
 	}
 
-	@AfterMapping
-	default void isMyTicket(Ticket entity, User user, @MappingTarget TicketResponseDto ticketResponseDto) {
-		// ticketResponseDto.setMyTicket(entity.getUser() != null && entity.getUser().getEmail().equals(user.getEmail()));
+	@Named("checkProgress")
+	default double checkProgress(Ticket ticket) {
+		LocalDateTime currentDate = LocalDateTime.now();
+		if (ticket.getExpiredAt() == null) {
+			return 0.0d;
+		} else if (ticket.getCompletedAt() != null ||
+			currentDate.isAfter(ticket.getExpiredAt().plusDays(1).atStartOfDay())) {
+			return 100.0d;
+		} else {
+			long now = ChronoUnit.HOURS.between(ticket.getCreatedAt(), currentDate);
+			long total = ChronoUnit.HOURS.between(ticket.getCreatedAt(),
+				ticket.getExpiredAt().plusDays(1).atStartOfDay());
+			return (100 * now / (double)total);
+		}
 	}
 
-	@Named("checkAssigned")
-	default String checkAssigned(Ticket ticket) {
-		return ticket.getUser() == null ? null : ticket.getUser().getEmail();
+	/*
+	 * 1. 티켓 점수 산정 방식은 가중 선형 조합을 이용한다.
+	 * 2. 추후 상용화 시 abstract class로 돌리고 @Value를 이용해 yml파일로 가중치를 따로 관리한다.
+	 * 3. ticket에 달린 task 사라지면 ticket의 score도 바꾼다.
+	 */
+	@Named("calculateScore")
+	default double calculateScore(Ticket ticket) {
+		Task task = ticket.getTask();
+		if (ticket.getCompletedAt() == null) {
+			return 0;
+		} else {
+			double ticketProgress = ticket.getExpiredAt() == null ? 0.0d : checkTicketDuration(ticket);
+			double taskProgress = task.getExpiredAt() == null ? 0.0d : checkTicketDurationWithTask(ticket);
+
+			double difficultyWeight = 4;
+			double priorityWeight = 4;
+			double ticketProgressWeight = 1;
+			double taskProgressWeight = 1;
+
+			return (ticket.getDifficulty() / (double)task.getTotalDifficulty()) * difficultyWeight +
+				(ticket.getPriority() / (double)task.getTotalPriority()) * priorityWeight +
+				ticketProgress * ticketProgressWeight +
+				taskProgress * taskProgressWeight;
+		}
 	}
 
-	@Named("checkLabel")
-	default String checkLabel(Ticket ticket) {
-		return ticket.getLabel() == null ? null : ticket.getLabel().getLabelTitle();
+	/*
+	 * 1. 티켓과 task의 expiredAt의 자유도를 고려한다.
+	 */
+
+	default double checkTicketDuration(Ticket ticket) {
+		if (ticket.getCompletedAt() == null) {
+			return 0;
+		} else {
+			long now = ChronoUnit.MINUTES.between(ticket.getCreatedAt(), ticket.getCompletedAt());
+			long total = ChronoUnit.MINUTES.between(ticket.getCreatedAt(),
+				ticket.getExpiredAt().plusDays(1).atStartOfDay());
+			return (100 * now / (double)total);
+		}
 	}
 
-	@Named("checkTicket")
-	default String checkMovementTicket(Ticket ticket) {
-		return ticket.getStatus() == null ? null : ticket.getStatus().toString();
+	default double checkTicketDurationWithTask(Ticket ticket) {
+		if (ticket.getCompletedAt() == null) {
+			return 0;
+		} else {
+			long now = ChronoUnit.MINUTES.between(ticket.getCreatedAt(), ticket.getCompletedAt());
+			long total = ChronoUnit.MINUTES.between(ticket.getCreatedAt(),
+				ticket.getTask().getExpiredAt().plusDays(1).atStartOfDay());
+			return (100 * now / (double)total);
+		}
 	}
 
 	List<TicketResponseDto> toDtoList(List<Ticket> tickets);
