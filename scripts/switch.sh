f53d3ef50bce614bcb22ce546398811e649b7500
aihtnyc <111184537+aihtnyc-h@users.noreply.github.com>
2023-04-14 15:15:28 +0900

    Update switch.sh

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "> 현재 구동중인 Port 확인"
-CURRENT_PROFILE=$(curl -s http://43.201.195.87/profile)
+CURRENT_PROFILE=$(curl -s http://43.201.195.87/profile | grep -Eo 'set[12]')
 
 # Idle Profile 찾기: s1이 사용중이면 s2가 Idle
 if [ $CURRENT_PROFILE == set1 ]
@@ -28,19 +28,21 @@ echo "> Nginx Current Proxy Port: $PROXY_PORT"
 echo "> Nginx Reload"
 sudo service nginx reload
 
-echo "> 이전에 구동중인 애플리케이션 종료 전 대기"
-sleep 10
-
 echo "> 이전에 구동중인 애플리케이션 종료"
-IDLE_PID=$(sudo lsof -t -i:$CURRENT_PORT) 
-echo "> lsof 결과: $(sudo lsof -i:$CURRENT_PORT)" 
-if [ -z $IDLE_PID ]
+if [ -z $CURRENT_PORT ]
 then
-echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+  echo "> 현재 구동중인 포트가 없습니다. 종료할 애플리케이션 없음."
 else
-echo "> kill -15 $IDLE_PID"
-sudo kill -15 $IDLE_PID
-sleep 5
+  IDLE_PID=$(sudo lsof -t -i:$CURRENT_PORT) 
+  echo "> lsof 결과: $(sudo lsof -i:$CURRENT_PORT)" 
+  if [ -z $IDLE_PID ]
+  then
+    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+  else
+    echo "> kill -15 $IDLE_PID"
+    sudo kill -15 $IDLE_PID
+    sleep 5
+  fi
 fi
 
 exit 0
