9c00da5d22061d67e28858cb4477902d59e4fc12
Cho Youngjun <whdudwns7777@gmail.com>
2023-04-16 03:29:48 +0900

    fix: Cascade option 수정

--- src/main/java/com/ddalggak/finalproject/domain/label/entity/Label.java
+++ src/main/java/com/ddalggak/finalproject/domain/label/entity/Label.java
@@ -47,7 +47,7 @@ public class Label extends BaseEntity {
 	@BatchSize(size = 100)
 	private List<LabelUser> labelUserList = new ArrayList<>();
 
-	@OneToMany(mappedBy = "label", orphanRemoval = true)
+	@OneToMany(mappedBy = "label")
 	private List<Ticket> tickets = new ArrayList<>();
 
 	@Builder
