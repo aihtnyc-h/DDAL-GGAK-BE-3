1024c038bdb0fce605166cf4db55fa43bc0c92a1
Cho Youngjun <whdudwns7777@gmail.com>
2023-04-17 02:59:05 +0900

    fix: RequestDto 수정

--- src/main/java/com/ddalggak/finalproject/domain/comment/dto/CommentRequestDto.java
+++ src/main/java/com/ddalggak/finalproject/domain/comment/dto/CommentRequestDto.java
@@ -1,18 +1,16 @@
 package com.ddalggak.finalproject.domain.comment.dto;
 
-import javax.validation.constraints.NotNull;
+import com.ddalggak.finalproject.global.validation.RequestId;
 
-import com.ddalggak.finalproject.domain.comment.entity.Comment;
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-public class CommentRequestDto{
+public class CommentRequestDto {
 	@Schema(name = "ticket Id")
-	@NotNull(message = "ticket Id is required")
+	@RequestId
 	private Long ticketId;
 	private String comment;
 
